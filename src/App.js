1b6410d9a20c5363f1454f98b0363ca67ad9927d
Jason Corbett <jason.corbett01@gmail.com>
2023-02-26 15:55:05 -0600

    Created basic webpage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
+import HomePage from './pages/Login/homepage';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        
+        <Route path="/" element={ <HomePage/> } />
       </Routes>
     </div>
   );
