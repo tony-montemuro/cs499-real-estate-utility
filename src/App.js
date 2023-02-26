e7e2c00be2adbb06ba2f0ea07a25378cd10ddd1c
Luke <ldf0010@uah.edu>
2023-02-26 14:54:56 -0600

    Added a page for the showings listings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
+import ShowingsListing from './pages/Login/ShowingsListing';
 
 function App() {
   return (
     <div className="App">
+      <h1>Start</h1>
       <Routes>
-        
+        <Route path="/showings" element={ <ShowingsListing/> } />
       </Routes>
     </div>
   );
