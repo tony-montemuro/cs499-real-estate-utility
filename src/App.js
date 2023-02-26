53e3293b192b6f810cea4fd02e8aadfd82922502
Tony Montemuro <tonyamontemuro@gmail.com>
2023-02-26 12:18:35 -0600

    routed detail pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import DetailedListing from "./pages/DetailedListing/DetailedListing";
+import DetailedShowing from "./pages/DetailedShowing/DetailedShowing";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        
+        <Route path="/listings/:listing" element={ <DetailedListing /> } />
+        <Route path="/showings/:showing" element={ <DetailedShowing /> } />
       </Routes>
     </div>
   );
