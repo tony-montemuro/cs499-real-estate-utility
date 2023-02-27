622bcf2251ba0aad32204c8f14068b5e9023b018
Simone Booth <112595401+S-Booth@users.noreply.github.com>
2023-02-26 19:26:33 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
-
+import PropertyListing from "./pages/PropertyListings";
 function App() {
   return (
     <div className="App">
       <Routes>
-        
+        <Route path="/propertylisting" element={ <PropertyListing />} />
       </Routes>
     </div>
   );
