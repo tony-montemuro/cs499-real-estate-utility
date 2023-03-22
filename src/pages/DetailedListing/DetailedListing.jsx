7a332abb65fe483095ddd41aad25b249d2bcd853
seb <112595401+NarwhalPotato@users.noreply.github.com>
2023-03-22 15:48:05 -0500

    Adds content to Detailed Listing Page

--- src/pages/DetailedListing/DetailedListing.jsx
+++ src/pages/DetailedListing/DetailedListing.jsx
@@ -1,6 +1,43 @@
+import "./DetailedListing.css";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import FrontendHelper from "../../util/FrontendHelper";
+import DetailedListingsLogic from "./DetailedListing.js";
+
 function DetailedListing() {
   return (
-    <h1>Detailed Listing Page</h1>
+    <>
+      <div className="detailed-listings-header">
+        <h1>Detailed Property Listing</h1>
+      </div>
+
+      { /* Render the body of this component if listings is defined. Otherwise, render a loading component. */ }
+      { /* need to set up full infomation pull from database */}
+      
+      <div class="container">
+        <div class="left">
+          <p>Images</p>
+          <div className="rectangle"/>
+          <div className="image2"/>
+          <div className="image3"/>
+        </div>
+        <div class="right">
+          <p>Property Details</p>
+          <h2>Price &emsp; bed | bath | sq ft</h2>
+          <h2>Address</h2><br/>
+          <hr class="insert-line"/>
+          <h2>Listing Agency: Phone #</h2>
+          <h2>&emsp; &emsp; Address</h2>
+          <h2>Listing Agent: Phone #</h2>
+          <h2>&emsp; &emsp; Email</h2><br/>
+          <hr class="insert-line"/>
+          <h2>Overview:</h2><br/>
+          <hr class="insert-line"/>
+          <h2>Room Details:</h2>
+        </div>
+      </div>
+    </>
+
   );
 };
 
