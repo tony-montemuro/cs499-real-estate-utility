a466a78901133e8c0bd5c742a3e971bef6e5fbd6
Tony Montemuro <tonyamontemuro@gmail.com>
2023-04-01 23:06:17 -0500

    load agent from auth session; wrote some auth functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 /* ===== IMPORTS ===== */
 import "./App.css";
-import { ImagesContext } from "./Contexts";
+import { AgentContext, ImagesContext } from "./Contexts";
 import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import AppLogic from "./AppUtils.js";
 import REUNavbar from './ui/Navbar/Navbar.jsx';
 import DetailedListing from "./pages/DetailedListing/DetailedListing.jsx";
 import DetailedShowing from "./pages/DetailedShowing/DetailedShowing.jsx";
@@ -14,21 +16,36 @@ function App() {
   /* ===== VARIABLES ===== */
   const imagesCache = {};
 
+  /* ===== STATES & FUNCTIONS ===== */
+
+  // states and functions from the js file
+  const { agent, getCurrentAgent } = AppLogic();
+
+  /* ===== EFFECTS ===== */
+
+  // code that is executed when the application is first loaded
+  useEffect(() => {
+    getCurrentAgent();
+    // eslint-disable-next-line
+  }, []);
+
   /* ===== APP COMPONENT ===== */
   return (
-    <ImagesContext.Provider value={ { imagesCache } }>
-      <div className="App">
-        <REUNavbar />
-        <Routes>
-          <Route path="/" element={ <Home /> } />
-          <Route path="/forms" element={ <Forms /> } /> 
-          <Route path="/listings" element={ <PropertyListing /> } />
-          <Route path="/listings/:listing" element={ <DetailedListing /> } />
-          <Route path="/showings" element={ <ShowingsListing /> } />
-          <Route path="/showings/:showing" element={ <DetailedShowing /> } />
-        </Routes>
-      </div>
-    </ImagesContext.Provider>
+    <AgentContext.Provider value={ { agent } }>
+      <ImagesContext.Provider value={ { imagesCache } }>
+        <div className="App">
+          <REUNavbar />
+          <Routes>
+            <Route path="/" element={ <Home /> } />
+            <Route path="/forms" element={ <Forms /> } /> 
+            <Route path="/listings" element={ <PropertyListing /> } />
+            <Route path="/listings/:listing" element={ <DetailedListing /> } />
+            <Route path="/showings" element={ <ShowingsListing /> } />
+            <Route path="/showings/:showing" element={ <DetailedShowing /> } />
+          </Routes>
+        </div>
+      </ImagesContext.Provider>
+    </AgentContext.Provider>
   );
 };
 
