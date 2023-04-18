bd773e6a66104ff893c0aa534dac0a2718466765
Tanner-cpu <71723237+Tanner-cpu@users.noreply.github.com>
2023-04-17 23:00:16 -0500

    Update SupabaseClient.js

--- src/database/SupabaseClient.js
+++ src/database/SupabaseClient.js
@@ -2,8 +2,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 /* ===== VARIABLES ===== */
-const API_URL = process.env.REACT_APP_SUPABASE_URL;
-const API_KEY = process.env.REACT_APP_SUPABASE_KEY;
+const API_URL = process.env.NEXT_PUBLIC_REACT_APP_SUPABASE_URL;
+const API_KEY = process.env.NEXT_PUBLIC_REACT_APP_SUPABASE_KEY;
 
 /* ===== EXPORTS ===== */
 export const supabase = createClient(API_URL, API_KEY);
