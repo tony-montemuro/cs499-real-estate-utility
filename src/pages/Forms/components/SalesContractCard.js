1b87a138f51d3a566abcb243cb95770a193cdfda
Tony Montemuro <tonyamontemuro@gmail.com>
2023-04-23 14:36:08 -0500

    moved all assets to asset folder

--- src/pages/Forms/components/SalesContractCard.js
+++ src/pages/Forms/components/SalesContractCard.js
@@ -1,4 +1,4 @@
-import pa_file from "../assets/pa.PNG";
+import pa_file from "../../../assets/pa.PNG";
 
 function SalesContractCard() {
     return <img src={pa_file} alt="Estimated closing template" />;
